3ddbcf2434d1e49a08a838d40d94b0be9cb9e95a
dandinglong <zhangchungame@163.com>
2019-12-08 21:17:33 +0800

    Initial commit

---
+++ src/main/java/com/dandinglong/springcloudforstudentadmin/SpringCloudForStudentAdminApplication.java
@@ -0,0 +1,13 @@
+package com.dandinglong.springcloudforstudentadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudForStudentAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudForStudentAdminApplication.class, args);
+    }
+
+}
